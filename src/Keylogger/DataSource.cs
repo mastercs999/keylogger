42e9c402f25c96db2863952b134c27a149b650bb
d3w201 <ghetto23@live.it>
2018-08-17 18:46:07 +0200

    Little edit

--- src/Keylogger/DataSource.cs
+++ src/Keylogger/DataSource.cs
@@ -1,11 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
 
